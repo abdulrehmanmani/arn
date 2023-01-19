a4d5658d9bc94f8816b1a45e200d37443a4f57f2
Muhammad Rehman Nasir <105709582+abdulrehmanmani@users.noreply.github.com>
2023-01-19 17:38:36 +0500

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -25,9 +25,7 @@ const Footer = ({}) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Made With ❤ by{" "}
-        <Link href="http://www.chetanverma.com">
-          <a className="underline underline-offset-1">Chetan Verma</a>
-        </Link>
+       hmftj & ARN
       </h1>
     </>
   );
